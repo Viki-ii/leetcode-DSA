af8789fa36e9a3173be8eb875677a49f120f6147
Vishnu P G <83322020+Viki-ii@users.noreply.github.com>
2026-08-06 15:25:47 +0530

    Time: 7 ms (26.51%) | Memory: 20.8 MB (32.65%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        n = len(prices)
+        BUY = 0
+        SELL = 1
+
+        dp = []
+        for i in range(n):
+            dp.append([-1] * 2)
+
+
+        def f(idx, transactionType):
+
+            if idx >= n :
+                return 0
+            
+            if dp[idx][transactionType] != -1:
+                return dp[idx][transactionType]
+
+            ans1 = f(idx + 1, transactionType)
+
+            if transactionType == BUY:
+                ans2 = -prices[idx] + f(idx + 1 ,SELL)
+            else:
+                ans2 = prices[idx] + f(idx + 2, BUY)
+
+            dp[idx][transactionType] = max(ans1, ans2)
+            return dp[idx][transactionType]
+        return f(0, BUY)
